837937a682196de7baaeb8db4eb9bda0f58d70ad
Benjamin Dean <benjamin@benjamindean.com>
2016-05-23 10:00:08 -0700

    Build up the code for this to operate as expected

---
+++ index.js
@@ -0,0 +1,185 @@
+'use strict';
+
+// Deps
+require('dotenv').config(); // If using this in multi-environment (local and deployed versions) might want to add logic for checking NODE_ENV environment variable to load only if local
+var RC = require('ringcentral');
+var Helpers = require('ringcentral-helpers');
+
+// Initialize the RC SDK
+var sdk = new RC({
+    server: process.env.RC_API_BASE_URL,
+    appKey: process.env.RC_APP_KEY,
+    appSecret: process.env.RC_APP_SECRET,
+    cachePrefix: process.env.RC_CACHE_PREFIX
+});
+
+// APP VARS
+var Message = Helpers.message();
+var platform = sdk.platform();
+var platform_SA = sdk_SA.platform();
+var subscription = sdk.createSubscription();
+
+// Login to the RingCentral Platform
+function login() {
+    return platform.login({
+            username: process.env.RC_USERNAME,
+            password: process.env.RC_PASSWORD,
+            extension: process.env.RC_EXTENSION
+        })
+        .then(function (response) {
+            console.log("Succesfully logged into the RC Account");
+            //console.log("The RC auth object is :", JSON.stringify(response.json(), null, 2));
+            init();
+        })
+        .catch(function (e) {
+            console.error("ERROR: ", e);
+            throw e;
+        });
+}
+
+function init() {
+    var extensions = [];
+    var page = 1;
+
+    function getExtensionsPage() {
+
+        return platform
+            .get('/account/~/extension', {
+                page: page,
+                perPage: process.env.EXTENSIONS_PER_PAGE //REDUCE NUMBER TO SPEED BOOTSTRAPPING
+            })
+            .then(function (response) {
+                //console.log("The extension response contained:", JSON.stringify(response.json(), null, 2));
+                var data = response.json();
+                console.log("************** THE NUMBER OF EXTENSIONS ARE : ***************", data.records.length);
+                extensions = extensions.concat(data.records);
+                if (data.navigation.nextPage) {
+                    page++;
+                    return getExtensionsPage(); // this will be chained
+                } else {
+                    return devices; // this is the finally resolved thing
+                }
+            });
+
+    }
+
+    /*
+     Loop until you capture all extensions
+     */
+    return getExtensionsPage()
+        .then(function (extensions) {
+            console.log("************** The total extensions are : **********", extensions.length);
+            return extensions.filter(getPhysicalDevices);
+        })
+        .then(createEventFilter)
+        .then(startSubscription)
+        .catch(function (e) {
+            console.error(e);
+            throw e;
+        });
+
+}
+
+function createEventFilter(extensions) {
+    console.log("********* CREATING EVENT FILTERS ***************");
+    var _eventFilters = [];
+    for(var i = 0; i < extensions.length; i++) {
+        var extension = extensions[i];
+        _eventFilters.push(generatePresenceEventFilter(extension));
+    }
+    return _eventFilters;
+}
+
+function generatePresenceEventFilter(item) {
+    //console.log("The item is :", item);
+    if (!item) {
+        ;
+        throw new Error('Message-Dispatcher Error: generatePresenceEventFilter requires a parameter');
+    } else {
+        //console.log("The Presence Filter added for the extension :" + item.extension.id + ' : /account/~/extension/' + item.extension.id + '/presence?detailedTelephonyState=true');
+        return '/account/~/extension/' + item.extension.id + '/presence?detailedTelephonyState=true';
+    }
+}
+
+function startSubscription(eventFilters) { //FIXME MAJOR Use devices list somehow
+    console.log("********* STARTING TO CREATE SUBSCRIPTION ON ALL FILTERED DEVICES ***************");
+    return subscription
+        .setEventFilters(eventFilters)
+        .register();
+}
+
+// Register Platform Event Listeners
+platform.on(platform.events.loginSuccess, handleLoginSuccess);
+platform.on(platform.events.loginError, handleLoginError);
+platform.on(platform.events.logoutSuccess, handleLogoutSuccess);
+platform.on(platform.events.logoutError, handleLogoutError);
+platform.on(platform.events.refreshSuccess, handleRefreshSuccess);
+platform.on(platform.events.refreshError, handleRefreshError);
+
+// Register Subscription Event Listeners
+subscription.on(subscription.events.notification, handleSubscriptionNotification);
+subscription.on(subscription.events.removeSuccess, handleRemoveSubscriptionSuccess);
+subscription.on(subscription.events.removeError, handleRemoveSubscriptionError);
+subscription.on(subscription.events.renewSuccess, handleSubscriptionRenewSuccess);
+subscription.on(subscription.events.renewError, handleSubscriptionRenewError);
+subscription.on(subscription.events.subscribeSuccess, handleSubscribeSuccess);
+subscription.on(subscription.events.subscribeError, handleSubscribeError);
+
+// Define Event Handlers
+function handleSubscriptionNotification(msg) {
+    console.log('SUBSCRIPTION NOTIFICATION DATA: ', data);
+}
+
+function handleRemoveSubscriptionSuccess(data) {
+    console.log('REMOVE SUBSCRIPTION SUCCESS DATA: ', data);
+}
+
+function handleRemoveSubscriptionError(data) {
+    console.log('REMOVE SUBSCRIPTION ERROR DATA: ', data);
+}
+
+function handleSubscriptionRenewSuccess(data) {
+    console.log('RENEW SUBSCRIPTION SUCCESS DATA: ', data);
+}
+
+function handleSubscriptionRenewError(data) {
+    console.log('RENEW SUBSCRIPTION ERROR DATA: ', data);
+}
+
+function handleSubscribeSuccess(data) {
+    console.log('SUBSCRIPTION CREATED SUCCESSFULLY');
+}
+
+function handleSubscribeError(data) {
+    console.log('FAILED TO CREATE SUBSCRIPTION: ', data);
+}
+
+/**
+ * Platform Event Handlers
+ **/
+function handleLoginSuccess(data) {
+    // UNCOMMENT TO VIEW LOGIN DATA
+    //console.log('LOGIN SUCCESS DATA: ', data);
+}
+
+function handleLoginError(data) {
+    console.log('LOGIN FAILURE DATA: ', data);
+}
+
+function handleLogoutSuccess(data) {
+    console.log('LOGOUT SUCCESS DATA: ', data);
+}
+
+function handleLogoutError(data) {
+    console.log('LOGOUT FAILURE DATA: ', data);
+}
+
+function handleRefreshSuccess(data) {
+    console.log('REFRESH SUCCESS DATA: ', data);
+}
+
+function handleRefreshError(data) {
+    console.log('REFRESH FAILURE DATA: ', data);
+    console.log('Initialing Login again :');
+    login();
+}
